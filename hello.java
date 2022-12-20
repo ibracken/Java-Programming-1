6e620a664e9ad1f127d86db9f909f32607a4743c
Ian Bracken <ibracken@ad.unc.edu>
2022-12-20 18:44:38 -0500

    first

---
+++ hello.java
@@ -0,0 +1,12 @@
+
+public class hello {
+
+    public static void main(String[] args) {
+        int dividend = 3;
+        int divisor = 2;
+
+        double result = 1.0 * dividend / divisor;
+        System.out.println(result);
+        System.out.println("That's a number");
+}
+}
